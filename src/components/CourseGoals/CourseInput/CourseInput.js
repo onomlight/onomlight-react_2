dd6c630215c6bce740dfd0b6a297d02ef2e0b3fa
onomlight <pass041@nate.com>
2022-12-28 18:35:19 +0900

    동적으로 css추가하기

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -28,14 +28,9 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{
-          //props 값을 동적으로 바꿈 
-          color:!isValid?'red':'black'
-          }}>Course Goal</label>
-        <input style={{borderColor: !isValid?'red':'black', 
-         background : !isValid?'salmon':'transparent' }} 
-         type="text" onChange={goalInputChangeHandler} />
+      <div className={`form-control ${!isValid ? 'invalid':''}`}>
+        <label >Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
