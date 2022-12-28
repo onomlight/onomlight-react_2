51f7049a84465ade98260becc7957090fc364418
onomlight <pass041@nate.com>
2022-12-28 21:16:41 +0900

    Styled Components & 동적 Props

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,45 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
+
+// @으로 사용하고싶으면 & input 안에 border 끝에 ${props => props.ivalid ? 'red':'#ccc'} 사용하면 사용가능함
+// back 그라운드도 동일
+const FormControl = styled.div`
+
+  margin: 0.5rem 0;
+
+
+& label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+& input {
+  display: block;
+  width: 100%;
+  border: 1px solid #ccc;
+  font: inherit;
+  line-height: 1.5rem;
+  padding: 0 0.25rem;
+}
+
+& input:focus {
+  outline: none;
+  background: #fad0ec;
+  border-color: #8b005d;
+}
+
+&.invalid input{
+  border-color: red;
+  background-color: #ffd7d7;
+}
+&.invalid label{
+  color: red;
+}
+`;
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -28,10 +66,12 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid':''}`}>
+      <FormControl className={!isValid && 'invalid'}
+      // @ invalid={!isValid}
+      >
         <label >Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </div>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
